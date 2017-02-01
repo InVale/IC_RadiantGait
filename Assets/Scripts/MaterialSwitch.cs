47d217306742d3c698ba8b617c7c8b896aabef2c
Marie <mariekoller@yahoo.fr>
2017-02-01 11:55:52 +0100

    Clean Up
    Cleaned up the files and created good prefabs.

---
+++ Assets/Scripts/MaterialSwitch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwitch : MonoBehaviour {
+
+    Material _lit;
+    public Material Unlit;
+    bool _isLit = true;
+
+    void Start()
+    {
+        _lit = gameObject.GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Swap();
+        }
+    } 
+
+    public void Swap ()
+    {
+        if (_isLit)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = Unlit;
+            _isLit = false;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material = _lit;
+            _isLit = true;
+        }
+    }
+}
